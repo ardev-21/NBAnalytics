1610bed2a7a319cf34ab777ac7a190cc368297b4
Nahuel Undabarrena <44739046+NahuelUnda@users.noreply.github.com>
2025-04-30 18:01:57 -0300

    Add files via upload

--- NBA_DB.sql
+++ NBA_DB.sql
@@ -1,8 +1,37 @@
+
+
+
+
+
+
+
+/*
+DROP TABLE Player;
+DROP TABLE Team;
+DROP TABLE Game;
+DROP TABLE Common_player_info;
+DROP TABLE Draft_combine_stats;
+DROP TABLE Draft_history;
+DROP TABLE Game_info;
+DROP TABLE Game_summary;
+DROP TABLE Inactive_players;
+DROP TABLE Officials;
+DROP TABLE Other_stats;
+DROP TABLE Play_by_play;
+DROP TABLE Team_details;
+DROP TABLE Team_history;
+DROP TABLE Team_info_common;
+*/
+
+
+
+
 CREATE TABLE Player (
 id INT PRIMARY KEY,
 full_name varchar(101),
 first_name varchar(50),
-last_name varchar(50)
+last_name varchar(50),
+is_active char(1)
 )
 
 
@@ -46,6 +75,7 @@ tov_home int,
 pf_home int,
 pts_home int,
 plus_minus_home int,
+video_available_home char(1),
 team_id_away int FOREIGN KEY REFERENCES Team(id) NOT NULL,
 team_abbreviation_away char(3),
 team_name_away varchar(50),
@@ -78,12 +108,7 @@ season_type varchar(25)
 CREATE TABLE Common_player_info (
 
 person_id INT PRIMARY KEY FOREIGN KEY REFERENCES Player(id),
-first_name varchar(50),
-last_name varchar(50),
-display_first_last varchar(101),
-display_last_comma_first varchar(101),
 display_fi_last varchar(53),
-player_slug varchar(101),
 birthdate DATETIME,
 school varchar(45),
 country varchar(45),
@@ -91,15 +116,9 @@ last_affiliation varchar(45),
 height varchar(5),
 weight INT,
 season_exp INT,
-jersey INT,
 position varchar(20),
 rosterstatus varchar(9),
-games_played_current_season_flag INT,
 team_id INT FOREIGN KEY REFERENCES Team(id),
-team_name varchar(25),
-team_abbreviation char(3),
-team_code varchar(20),
-team_city varchar(25),
 player_code varchar(50),
 from_year INT,
 to_year INT,
@@ -122,45 +141,15 @@ last_name varchar(50),
 player_name varchar(101),
 position varchar(5),
 height_wo_shoes DECIMAL(6,2),
-height_wo_shoes_ft_in varchar(15),
-height_w_shoes DECIMAL (6,2),
-height_w_shoes_ft_in varchar(15),
 weight DECIMAL(5,1),
 wingspan DECIMAL(5,2),
-wingspan_ft_in varchar(15),
 standing_reach DECIMAL (5,1),
-standing_reach_ft_in varchar(15),
 body_fat_pct DECIMAL(5,2),
-hand_lenght DECIMAL(3,1),
-hand_width DECIMAL(3,1),
 standing_vertical_leap DECIMAL(5,2),
 max_vertical_leap DECIMAL(5,2),
 lane_agility_time DECIMAL(5,2),
-modified_lane_agility_time DECIMAL(5,2),
 three_quarter_sprint DECIMAL(4,2),
 bench_press INT,
-spot_fifteen_corner_left varchar(5),
-spot_fifteen_break_left varchar(5),
-spot_fifteen_top_key varchar(5),
-spot_fifteen_break_right varchar(5),
-spot_fifteen_corner_right varchar(5),
-spot_college_corner_left varchar(5),
-spot_college_break_left varchar(5),
-spot_college_top_key varchar(5),
-spot_college_break_right varchar(5),
-spot_college_corner_right varchar(5),
-spot_nba_break_left varchar(5),
-spot_nba_top_key varchar(5),
-spot_nba_break_right varchar(5),
-spot_nba_corner_right varchar(5),
-off_drib_fifteen_break_left varchar(5),
-off_drib_fifteen_top_key varchar(5),
-off_drib_fifteen_break_right varchar(5),
-off_drib_college_break_left varchar(5),
-off_drib_college_top_key varchar(5),
-off_drib_college_break_right varchar(5),
-on_move_fifteen varchar(7),
-on_move_college varchar(7)
 )
 
 
@@ -193,10 +182,7 @@ arenacapacity INT,
 owner varchar(50),
 generalmanager varchar(50),
 headcoach varchar(50),
-dleagueaffiliation varchar(65),
-facebook nvarchar(255),
-instagram nvarchar(255),
-twitter nvarchar(255),
+dleagueaffiliation varchar(65)
 )
 
 
@@ -210,6 +196,7 @@ year_active_till INT
 )
 
 
+/*
 CREATE TABLE Team_info_common(
 team_id INT PRIMARY KEY FOREIGN KEY REFERENCES Team(id),
 season_year INT,
@@ -238,13 +225,11 @@ ast_pg INT,
 opp_pts_rank INT,
 opp_pts_pg INT
 )
-
+*/
 
 CREATE TABLE Game_info (
 game_id INT PRIMARY KEY FOREIGN KEY REFERENCES Game(game_id),
 game_date DATETIME,
-attendance INT,
-game_time VARCHAR (25) --NO SE SI ES HORARIO DE PARTIDO O UNA MEDICINO DE TIEMPO ASI QUE NOSE SI DEBERIA IR VARCHAR O TIME
 )
 
 
@@ -259,8 +244,6 @@ home_team_id INT FOREIGN KEY REFERENCES Team(id),
 visitor_team_id INT FOREIGN KEY REFERENCES Team(id),
 season INT,
 live_period INT,
-live_pc_time varchar (10),
-natl_tv_broadcaster_abbreviation varchar(15),
 live_period_time_bcast varchar(20),
 wh_status char(1)
 )
@@ -268,7 +251,6 @@ wh_status char(1)
 
 CREATE TABLE Other_stats(
 game_id INT PRIMARY KEY FOREIGN KEY REFERENCES Game(game_id),
-league_id INT,
 team_id_home INT FOREIGN KEY REFERENCES Team(id),
 team_abbreviation_home char(3),
 team_city_home varchar(25),
@@ -301,16 +283,15 @@ game_id INT PRIMARY KEY FOREIGN KEY REFERENCES Game(game_id),
 official_id INT,
 first_name varchar(20),
 last_name varchar(35),
-jersey_num INT
 )
 
 
 CREATE TABLE Inactive_players(
 log_id INT PRIMARY KEY IDENTITY(1,1),
+game_id INT FOREIGN KEY REFERENCES Game(game_id),
 player_id INT FOREIGN KEY REFERENCES Player(id),
 first_name varchar(25),
 last_name varchar(25),
-jersey_num INT,
 team_id INT FOREIGN KEY REFERENCES Team(id),
 team_city varchar(25),
 team_name varchar(25),
@@ -321,38 +302,52 @@ team_abbreviation char(3)
 CREATE TABLE Play_by_play(
 play_id INT IDENTITY(1,1) PRIMARY KEY,
 game_id INT FOREIGN KEY REFERENCES Game(game_id),
-eventnum INT,
 eventmsgtype INT,
 eventmsgactiontype INT,
-period INT,
-wctimestring TIME,
 pctimestring varchar(10),
 homedescription varchar(140),
 neutraldescription varchar(140),
 visitordescription varchar(140),
 score varchar(10),
 scoremargin INT,
-person1type DECIMAL(10,2),
 player1_id INT FOREIGN KEY REFERENCES Player(id),
-player1_name varchar(25),
 player1_team_id INT FOREIGN KEY REFERENCES Team(id),
-player1_team_city varchar(25),
-player1_team_nickname varchar(25),
-player1_team_abbreviation char(3),
-person2type DECIMAL(10,2),
 player2_id INT FOREIGN KEY REFERENCES Player(id),
-player2_name varchar(25),
 player2_team_id INT FOREIGN KEY REFERENCES Team(id),
-player2_team_city varchar(25),
-player2_team_nickname varchar(25),
-player2_team_abbreviation char(3),
-person3type DECIMAL(10,2),
 player3_id INT FOREIGN KEY REFERENCES Player(id),
-player3_name varchar(25),
 player3_team_id INT FOREIGN KEY REFERENCES Team(id),
-player3_team_city varchar(25),
-player3_team_nickname varchar(25),
-player3_team_abbreviation char(3),
-video_available_flag char(1)
 )
 
+CREATE TABLE line_score (
+game_date_est DATETIME,
+game_sequence INT,
+game_id INT PRIMARY KEY FOREIGN KEY REFERENCES Game(game_id),
+team_id_home INT FOREIGN KEY REFERENCES Team(id),
+team_abbreviation_home varchar(25),
+team_city_name_home varchar(25),
+team_nickname_home varchar(25),
+team_wins_losses_home varchar(10),
+pts_qtr1_home int,
+pts_qtr2_home int,
+pts_qtr3_home int,
+pts_qtr4_home int,
+pts_ot1_home int,
+pts_ot2_home int,
+pts_ot3_home int,
+pts_ot4_home int,
+pts_home int,
+team_id_away INT FOREIGN KEY REFERENCES Team(id),
+team_abbreviation_away varchar(25),
+team_city_name_away varchar(25),
+team_nickname_away varchar(25),
+team_wins_losses_away varchar(10),
+pts_qtr1_away int,
+pts_qtr2_away int,
+pts_qtr3_away int,
+pts_qtr4_away int,
+pts_ot1_away int,
+pts_ot2_away int,
+pts_ot3_away int,
+pts_ot4_away int,
+pts_away int
+)
